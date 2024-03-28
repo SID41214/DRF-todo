bd6045331bc96c9134c0a075960460db820865a7
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-03-28 13:10:22 +0530

    updated

--- todo/api/views.py
+++ todo/api/views.py
@@ -14,3 +14,6 @@ class RegisterAPIView(GenericAPIView):
             return response.Response(serializer.data,status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
+    
+class LoginAPIView(GenericAPIView):
+    pass
